9fba6b0183bcb728b472efde3ea6a845ae37c53e
Hanck1215 <st11025212@gmail.com>
2023-07-04 00:33:47 +0800

    創建rabbit物件

--- 物件練習.js
+++ 物件練習.js
@@ -2,3 +2,6 @@ function Rabbit(type) { this.type = type ; }
 Rabbit.prototype.speak = function(line) {
     console.log(`The ${this.type} rabbit says '${line}'.`) ;
 } ;
+
+
+let rabbit = new Rabbit("none") ;
